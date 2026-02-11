0ca2d21a048d94092676a330add36a681e5c6f8b
Joao Artur Peixoto <joao.peixoto.artur@gmail.com>
2026-02-11 18:20:58 -0300

    Ajuste definitivo do caminho dos dados

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function DashboardIBGE() {
     async function carregarDados() {
       try {
         // A "chamada" para o prédio do IBGE
-        const res = await fetch('https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/2022/variaveis/9324?localidades=N2[all]');
+        const res = await fetch('https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/2022/variaveis/9324?localidades=N1[all]');
         const json = await res.json();
         
         // AQUI É O AJUSTE DO CAMINHO:
